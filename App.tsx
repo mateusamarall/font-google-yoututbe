01c76213d0cffb1ada86e2dfb4b0dd9d28c2416b
mateusamarall <mateus.amaral018@gmail.com>
2021-01-11 20:36:04 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Poppins_400Regular, Poppins_600SemiBold, useFonts} from '@expo-google-fonts/poppins';
+import {AppLoading} from 'expo';
 
+//SDK 40
+//yarn add expo-app-loading;
+//import AppLoading from 'expo-app-loading';
 export default function App() {
+
+  const fonts = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
+
+  if(!fonts){
+    return <AppLoading/>
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.titulo}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +32,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo:{
+    fontFamily:"Poppins_600SemiBold",
+    fontSize:18
+  }
 });
